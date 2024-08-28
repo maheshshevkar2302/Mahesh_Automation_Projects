674b8e58931ec3a5e0a28a79e7f18880399c0181
mahesh@gmail.com <mahesh@gmail.com>
2024-08-28 14:43:14 +0530

    gettersetter

---
+++ src/Getter_Setter_Method/Class_Student.java
@@ -0,0 +1,45 @@
+package Getter_Setter_Method;
+
+public class Class_Student 
+{
+	private String StudentName;
+	private String StudentCity;
+	
+	public String getStudentName() 
+	{
+		
+		return this.StudentName;
+		
+	}
+	//Getter method is used to retrieve data 
+	
+	
+	public void setStudentName(String StudentName) 
+	{
+		
+		this.StudentName=StudentName;
+		
+	}
+
+
+	public String getStudentCity() {
+		return StudentCity;
+	}
+
+
+	public void setStudentCity(String StudentCity) {
+		this.StudentCity = StudentCity;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/Getter_Setter_Method/Main_Class.java
@@ -0,0 +1,28 @@
+package Getter_Setter_Method;
+
+public class Main_Class {
+	
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		Class_Student  St1=new Class_Student ();
+		
+	
+		
+		St1.setStudentCity("Mahesh");
+		
+		St1.setStudentName("Yavatmal");
+		
+	String City=St1.getStudentCity();
+	System.out.println(City);
+	
+
+	String Address=St1.getStudentName();
+	System.out.println(Address);
+	
+		
+	}
+
+}
